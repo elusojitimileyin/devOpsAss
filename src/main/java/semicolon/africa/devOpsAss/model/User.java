f616dee9123a34a6cc082f64b79efb57f7d1897e
Elusoji Timileyin <elusojitimileyin@gmail.com>
2024-08-27 14:05:58 +0100

    login user

--- src/main/java/semicolon/africa/devOpsAss/model/User.java
+++ src/main/java/semicolon/africa/devOpsAss/model/User.java
@@ -1,9 +1,11 @@
 package semicolon.africa.devOpsAss.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class User {
     @Id
     private Long id;
